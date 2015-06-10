1f5b786319e6dd3ec0e842965619d2ec98ebf08b
mingruilyu <davidlvmingrui@gmail.com>
2015-06-10 03:24:25 -0700

    Revert "change synchronized"
    This reverts commit b43bb0ae1a69684fd89302cc003e0c798d1e577a.

--- space/CheckPointTimer.java
+++ space/CheckPointTimer.java
@@ -28,9 +28,9 @@ public class CheckPointTimer extends Timer{
 		@Override
 		public void run() {
 			System.out.println("Checkpoint running!");
-			synchronized(this.jobContext.shadow) {
+			synchronized(this.jobContext.readyQueue) {
 				synchronized(this.jobContext.waitingQueue) {
-					synchronized(this.jobContext.readyQueue) {
+					synchronized(this.jobContext.shadow) {
 						try {
 							this.mirror.checkPoint(this.jobContext, jobId);
 							System.out.println("Checkpoint!");
